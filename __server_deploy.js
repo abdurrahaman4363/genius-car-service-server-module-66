ff0b45d7a4af41bc833e814ccbdcf04ad568326e
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2022-04-27 23:01:11 +0600

    added heroku

---
+++ __server_deploy.js
@@ -0,0 +1,32 @@
+/**
+ * ---------------------------
+ * ONE TIME for your computer
+ * ---------------------------
+ * 1. create heroku account
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. heroku login 
+ * ---------------------------
+ *  for each project one time
+ * ---------------------------
+ * 1. heroku create
+ * 2. make sure you : 1. git add.
+ *                    2. git commit -m"write something"
+ *                    3. git push
+ * 3. git push heroku main
+ * 4. Go to Heroku dashboard > current project > settings > Reveal config vars
+ * 5. copy paste config vars from your .env file
+ * 6. make sure you have whitelisted all ip address to access mongodb
+ * ------------------------------
+ * UPDATE SERVER with new changes
+ * ------------------------------
+ * 1. make changes
+ * 2. make sure you :1. git add .
+ *                   2. git commit -m""
+ *                   3. git push
+ * 3. git push heroku main                 
+ * -------------------------
+ * Connect server with Client
+ * --------------------------
+ * 
+ **/
